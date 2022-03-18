6f405e2302ee99a7724ee90547a70cc9f09237bd
Sankaranarayanan Sengunther <shankar.muthusamy4@gmail.com>
2022-03-18 23:57:54 +0530

    about us completed (refer this project for css{jsm}

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className='app__wrapper_info'>
       <SubHeading title="Chase The New Flavour" />
+      
       <h1 className='app__header-h1'> The Key to Fine Dining </h1>
+
       <p className='p__opensans' style={{ margin: '2rem 0' }}>
         Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus
       </p>
@@ -15,8 +17,7 @@ const Header = () => (
     </div>
 
     <div className='app__wrapper_img'>
-      <img src={images.welcome} alt='header-img' />
-    </div>
+      <img src={images.welcome} alt='header-img'/> </div>
 
   </div>
 );
